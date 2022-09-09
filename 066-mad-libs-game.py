2f3603fb3595272dcac161a51e3f776e59adea93
haraGADygyl <ti60gtt@gmail.com>
2022-09-09 17:21:13 +0300

    add 066

---
+++ 066-mad-libs-game.py
@@ -0,0 +1,5 @@
+color = input('Enter a color: ')
+plural_noun = input('Enter a plural noun: ')
+celebrity = input('Enter a celebrity: ')
+
+print(f'Roses are {color}\n{plural_noun} are blue\nI like {celebrity}')
